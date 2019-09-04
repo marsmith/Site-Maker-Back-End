47d129c1f9bf7d6875f24db37a690dea8e7043bc
Marcus <marcuspanozzo@gmail.com>
2019-09-04 17:03:56 -0400

    test update 9-4-19

---
+++ Tests/Test002_Loops.py
@@ -0,0 +1,14 @@
+import CreateStructure as CS
+
+if __name__ == "__main__":
+    dictt = CS.importJSON("../Data/LoopTest001-NHDSubset.json")
+    net = CS.isolateNet(dictt)    
+    sinks = CS.calculateSink(net)
+    CS.removeUseless(net)
+    assert(len(sinks) == 1)
+    faucets = CS.calculateFaucets(net)
+    CS.calculateUpstreamDistances(net,faucets)
+    net.recalculateTotalLength()
+
+    CS.pSNA(net,CS.SiteID(1001,9999,None),sinks[0])
+    print("Done!")
