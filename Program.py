7abd47bc14d87e0e28de54bafd5a3b95805e229b
Marcus <marcuspanozzo@gmail.com>
2019-09-25 13:29:09 -0400

    Added gitignore

--- Program.py
+++ Program.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     site = Site(0,0,0,0)
     #PreChanges: 3:40
     net = NetworkCrafter.generateNetworks("Data/OneidaFlowSimplified.json")
+    
     print("Hey!")
     # pSNA(net,SiteID(1001,9999,None))
     # DataIO.networkToCSV(net,"C:\\Users\\mpanozzo\\Documents\\SITE_TABLE.csv","C:\\Users\\mpanozzo\\Documents\\FLOW_TABLE.csv")
