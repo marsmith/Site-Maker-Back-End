53f220f633a078df8a7b5f152df23f189b4c2aea
Marcus <marcuspanozzo@gmail.com>
2019-10-11 09:49:31 -0400

    Getting there

--- GDALCode.py
+++ GDALCode.py
@@ -5,57 +5,69 @@ from osgeo import gdal_array
 from osgeo import gdalconst
 import os
 
-def isolateNetwork(folderPath,siteLayerName,lineLayerName,userClickLayerName):
+def isolateNetwork(folderPath,siteLayerName,lineLayerName,x,y):
     # Load Lines
     path = folderPath + "\\" + lineLayerName + "\\" + lineLayerName + ".shp"
-    path_userClick = folderPath + "\\" + userClickLayerName + "\\" + userClickLayerName + ".shp"
+    
     path_sites = folderPath + "\\" + siteLayerName + "\\" + siteLayerName + ".shp"
-    dataSource = ogr.Open(path)
-    shpdriver = ogr.GetDriverByName('ESRI Shapefile')
-    layer = dataSource.GetLayer()
     
-
     
-
     # Buffer around userClick
     print("Now buffer around userClick")
-    pointDataSource = ogr.Open(path_userClick)
-    pl = pointDataSource.GetLayer()
-    assert (len(pl) == 1) # There should only be one user click inside the shapefile
-    point = pl.GetNextFeature()
+    oRef = osr.SpatialReference()
+    oRef.ImportFromEPSG(4326)
+
     # Reproject
     targRef = osr.SpatialReference()
     targRef.ImportFromEPSG(26918) # NY State Projection
     
-    # Get the pointlayer projection
-    point_spatialRef = pl.GetSpatialRef()
+    # Create point from x,y and targRef
+    ctran = osr.CoordinateTransformation(oRef,targRef)
+    [p_lng,p_lat,z] = ctran.TransformPoint(x,y)
+    inputPointProj = ogr.Geometry(ogr.wkbPoint)
+    inputPointProj.SetPoint_2D(0,p_lng,p_lat)
 
-    transform = osr.CoordinateTransformation(point_spatialRef,targRef)
 
+    print("I got to here")
 
-
-    ingeom = point.GetGeometryRef()
-    ingeom.Transform(transform)
-    geomBuffer = ingeom.Buffer(10.0) # Buffer 10 meters around the geometry
+    geomBuffer = inputPointProj.Buffer(10000) # Buffer 10 meters around the geometry
  
     # Load Selected Sites
     sitesDataSource = ogr.Open(path_sites)
     sl = sitesDataSource.GetLayer()
-
+    ctran = osr.CoordinateTransformation(sl.GetSpatialRef(),targRef)
+    
     # Intersect of BUFFER and SITES
     interSites = []
     for site in sl:
         ingeom_site = site.GetGeometryRef()
+        ingeom_site.Transform(ctran)
         if ingeom_site.Intersect(geomBuffer):
             # This site is inside the buffer!
             interSites.append(site)
 
     # IF #INTER_SITES < 2 (expand circle)?
+    print(len(interSites))
     if len(interSites) < 2:
         print("UUUUUUh")
-    # Load Selected Lines
+    
+    # Save sites that were inside the circle as a layer
 
+
+    # Load Selected Lines
+    dataSource = ogr.Open(path)
+    shpdriver = ogr.GetDriverByName('ESRI Shapefile')
+    linesLayer = dataSource.GetLayer()
+    interLines = []
     # Intersect of BUFFER and LINES
+    for line in linesLayer:
+        ingeom_line = line.GetGeometryRef()
+        ingeom_line.Transform(ctran)
+        if ingeom_line.Intersect(geomBuffer):
+            interLines.append(line)
+
+    
+
 
     # Split INTER_LINES based on points (INTER_SITES)
 
@@ -67,4 +79,4 @@ def isolateNetwork(folderPath,siteLayerName,lineLayerName,userClickLayerName):
 
 
 if __name__ == "__main__":
-   isolateNetwork("C:\\Users\\mpanozzo\\Desktop\\GDAL Data","SitesSnapped","NHDFlowline","USER_CLICK")
+   isolateNetwork("C:\\Users\\mpanozzo\\Desktop\\GDAL Data","SitesSnapped","NHDFlowline",-73.939645996,42.378012068)
